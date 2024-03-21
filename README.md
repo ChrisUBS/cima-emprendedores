29cd7085b940209a50d16080ab967e9c311df59c
Ale1188 <dramoizta@gmail.com>
2024-03-20 23:06:25 -0700

    Validacion de todos los campos, Boton registro funcional

--- README.md
+++
@@ -1,1 +0,0 @@
-"# cimarrones-emprendedores" 
--- scripts/app.js
+++ scripts/app.js
@@ -1,61 +1,140 @@
 var people = [];
 
 class Person {
-    constructor(name, lastName, middleName, email, matricula, noEmpleado) {
+    constructor(name, lastName, middleName, email, matricula, noEmpleado, ubicacion, facultad, taller) {
         this.name = name;
         this.lastName = lastName;
         this.middleName = middleName;
         this.email = email;
         this.matricula = matricula;
         this.noEmpleado = noEmpleado;
+        this.ubicacion = ubicacion;
+        this.facultad = facultad;
+        this.taller = taller;
     }
 }
 
 
-function isValid(person, ubicacion, facultad, taller) {
+function isValid(person) {
     let validation = true;
     $("#notifications").removeClass("alert-error");
     $("#notifications").removeClass("alert-success");
     
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (person.name === "" || person.lastName === "" || person.middleName === "" || person.email === "" || person.matricula === "" || person.noEmpleado === "" || ubicacion === "" || facultad === "" || taller === "") {
+    // Verificar si alguno de los campos requeridos está vacío
+    if (person.name === "" || person.lastName === "" || person.middleName === "" || person.email === "" || person.matricula === "" || person.noEmpleado === "" || person.ubicacion === "" || person.facultad === "" || person.taller === "") {
         validation = false;
         $(".input-control input").css("border-color", "initial");
+        // Marcar los campos de texto vacíos con borde rojo
         $(".input-control input").each(function () {
             if ($(this).val() === "") {
                 $(this).css("border-color", "red");
             }
         });
+        // Marcar los select de ubicacion, facultad y taller en rojo si no se ha seleccionado ninguna opción
+        if (person.ubicacion === "") {
+            $("#txtCampus").css("border-color", "red");
+        }
+        if (person.facultad === "") {
+            $("#txtFacultad").css("border-color", "red");
+        }
+        if (person.taller === "") {
+            $("#txtTaller").css("border-color", "red");
+        }
     }
 
-    // Verificar que el campo del email sea válido.
+    // Verificar si el campo del email es válido usando la expresión regular
     if (person.email !== "" && !emailRegex.test(person.email)) {
         validation = false;
-        $(".input-control  input#txtEmail").css("border-color", "red");
+        $(".input-control input#txtEmail").css("border-color", "red");
     }
 
-    // Timer para restablecer el borde rojo después de 1.5 segundos si es necesario.
+    // Restablecer el borde rojo después de 1.5 segundos si es necesario
     if (!validation) {
         setTimeout(function () {
             $(".input-control input").css("border-color", "initial");
+            $("#txtCampus").css("border-color", "initial");
+            $("#txtFacultad").css("border-color", "initial");
+            $("#txtTaller").css("border-color", "initial");
         }, 1500);
     }
     return validation;
 }
 
-// function validRegister(){
-//     var selectedUbicacion = $("#ubicacion select").val();
-//     var selectedFacultad = $("#facultad select").val();
-//     var selectedTaller = $("#taller select").val();
-// }
+function validNext(newPerson, selectedOption){
+    if (isValid(newPerson)){
+        validRegister(newPerson,selectedOption);
+        notifications("alert-success", "Registro exitoso");
+        console.log("Valor:", newPerson);
+    }else{
+        notifications("alert-error", "¡Campo requerido ó no valido!");
+    }
+}
+
+function validRegister(newPerson, selectedOption){
+    switch (selectedOption) {
+        case "option1":
+            Alumno();
+            $("#btnRegister").click(function(){
+                checkExtra(newPerson, selectedOption);
+                console.log("Valor:", newPerson);
+                if (isValid(newPerson)) {
+                    notifications("alert-success", "Registro exitoso");
+                    console.log("Valor:", newPerson);
+                } else {
+                    notifications("alert-error", "¡Campo requerido ó no valido!");
+                }
+            });
+            break;
+        case "option2":
+            Docente();
+            $("#btnRegister").click(function(){
+                checkExtra(newPerson, selectedOption);
+                console.log("Valor:", newPerson);
+                if (isValid(newPerson)) {
+                    notifications("alert-success", "Registro exitoso");
+                    console.log("Valor:", newPerson);
+                } else {
+                    notifications("alert-error", "¡Campo requerido ó no valido!");
+                }
+            });
+            break;
+        case "option3":
+            Exterior();
+            $("#btnRegister").click(function(){
+                checkExtra(newPerson, selectedOption);
+                console.log("Valor:", newPerson);
+                if (isValid(newPerson)) {
+                    notifications("alert-success", "Registro exitoso");
+                    console.log("Valor:", newPerson);
+                } else {
+                    notifications("alert-error", "¡Campo requerido ó no valido!");
+                }
+            });
+            break; 
+        case "option4":
+            Exterior();
+            $("#btnRegister").click(function(){
+                checkExtra(newPerson, selectedOption);
+                console.log("Valor:", newPerson);
+                if (isValid(newPerson)) {
+                    notifications("alert-success", "Registro exitoso");
+                    console.log("Valor:", newPerson);
+                } else {
+                    notifications("alert-error", "¡Campo requerido ó no valido!");
+                }
+            });
+            break;
+    }
+}
 
 function register() {
-    var selectedOption = $("#txtOption").val();
-    var inputName, inputLastName, inputMiddleName, inputEmail, inputMatricula, inputNoEmpleado;
+    let selectedOption = $("#txtOption").val();
+    let inputName, inputLastName, inputMiddleName, inputEmail, inputMatricula, inputNoEmpleado, selectUbicacion, selectFacultad, selectTaller;
     
     if (selectedOption === "option1") {
-        inputMatricula = $("input[name='matricula']").eq(0).val();
+        inputMatricula = $("input[name='matricula']").val();
         inputName = $("input[name='nombre']").val();
         inputLastName = $("input[name='apellidoP']").val();
         inputMiddleName = $("input[name='apellidoM']").val();
@@ -77,54 +156,30 @@ function register() {
         inputMiddleName = $("input[name='apellidoM']").val();
         inputEmail = $("input[name='email']").val();
     }
-    
-
-    let newPerson = new Person(inputName, inputLastName, inputMiddleName, inputEmail, inputMatricula, inputNoEmpleado);
-    var selectedUbicacion = $("#txtCampus").val();
-    var selectedFacultad = $("#txtFacultad").val();
-    var selectedTaller = $("#txtTaller").val();
-        
-    if (isValid(newPerson)) {
-        // displayPerson(newPerson);
-        // people.push(newPerson);
-        // notifications("alert-success", "Registro exitoso");
-        // if(isValid(newPerson, selectedUbicacion, selectedFacultad, selectedTaller)){
-        //     notifications("alert-success", "Registro exitoso");
-        // }else{
-        //     notifications("alert-error", "¡Campo requerido ó no valido!");
-        // }
-        //$("#btnRegister").click(console.log("Sirve"));
-        switch (selectedOption) {
-            case "option1":
-                Alumno();
-                $("#btnRegister").click(function(){
-                    if (isValid(alumno1,'tijuana', selectedFacultad, selectedTaller)) {
-                        notifications("alert-success", "Registro exitoso");
-                        console.log("entre")
-                    } else {
-                        notifications("alert-error", "¡Campo requerido ó no valido!");
-                    }
-                });
-                break;
-            case "option2":
-                Docente();
-                break;
-            case "option3":
-                Egresado();
-                break; 
-            case "option4":
-                Externo();
-                break;
-        }
-    } else {
-        notifications("alert-error", "¡Campo requerido ó no valido!");
-    }
-    
+    let newPerson = new Person(inputName, inputLastName, inputMiddleName, inputEmail, inputMatricula, inputNoEmpleado, selectUbicacion, selectFacultad, selectTaller);
+    validNext(newPerson, selectedOption);
 }
 
+function checkExtra(newPerson, selectedOption){
+    let selectUbicacion = $("#txtCampus").val();
+    let selectFacultad = $("#txtFacultad").val();
+    let selectTaller = $("#txtTaller").val();
+    if (selectedOption === "option1") {
+        newPerson.ubicacion = selectUbicacion;
+        newPerson.facultad = selectFacultad;
+        newPerson.taller = selectTaller;
+    } else if (selectedOption === "option2") {
+        newPerson.ubicacion = selectUbicacion;
+        newPerson.facultad = selectFacultad;
+        newPerson.taller = selectTaller;
+    } else if (selectedOption === "option3" || "option3") {
+        newPerson.ubicacion = selectUbicacion;
+        newPerson.taller = selectTaller;
+    }
+}
 
 //Opciones del 1 al 4.
-function Alumno(newPerson){
+function Alumno(){
     $('#txtFacultad').prop('disabled', true);
     $('#txtTaller').prop('disabled', true);
     $("#btnNext").hide();
@@ -158,22 +213,7 @@ function Docente(){
         $("#btnRegister").hide();
     });
 }
-function Egresado(){
-    $('#txtFacultad').prop('disabled', true);
-    $('#txtTaller').prop('disabled', true);
-    $("#btnNext").hide();
-    $("#btnRegister").show();
-    $("#ubicacion").show();
-    $("#taller").show();
-    $("#ubicacion select").change(Facultad);
-    $("#ubicacion select").change(Talleres);
-    $("#txtOption").change(function() {
-        $("#ubicacion").hide();
-        $("#taller").hide();
-        $("#btnRegister").hide();
-    });
-}
-function Externo(){
+function Exterior(){
     $('#txtFacultad').prop('disabled', true);
     $('#txtTaller').prop('disabled', true);
     $("#btnNext").hide();
@@ -189,22 +229,25 @@ function Externo(){
     });
 }
 
+
 //Facultad
 function Facultad() {
+    var selectedUbicacion = $("#txtCampus").val();
+    console.log("Selected campus:", selectedUbicacion);
     const facultadesXUbicacion = {
         "Tijuana": ["Facultad de Artes", "Facultad de Ciencias Químicas e Ingeniería", "Facultad de Contaduría y Administración", "Facultad de Ciencias de la Salud, Valle de las Palmas", "Facultad de Deportes", "Facultad de Derecho", "Facultad de Economía y Relaciones Internacionales", "Facultad de Humanidades y Ciencias Sociales", "Facultad de Idiomas", "Facultad de la Ingeniería y Tecnología", "Facultad de Medicina y Psicología", "Facultad de Odontología", "Facultad de Turismo y Mercadotecnia", "Instituto de Investigaciones Históricas", "Facultad de Ciencias de la Ingeniería, Administrativas y Sociales"],
         "Mexicali": ["Facultad de Artes", "Facultad de Arquitectura y Diseño", "Instituto de Ciencias Agrícolas", "Facultad de Ciencias Sociales y Políticas", "Facultad de Ciencias Humanas", "Facultad de Ciencias Administrativas", "Facultad de Deportes", "Facultad de Derecho", "Facultad de Enfermería", "Facultad de Idiomas", "Facultad de Ingeniería", "Facultad de Ingeniería y Negocios Guadalupe Victoria", "Instituto de Investigaciones en Ciencias Veterinarias", "Instituto de Investigaciones Culturales (IIC Museo)", "Instituto de Investigaciones Sociales", "Instituto de Ingeniería", "Facultad de Medicina", "Facultad de Odontología", "Facultad de Pedagogía e Innovación Educativa"],
         "Ensenada": ["Facultad de Artes", "Escuela de Ciencias de la Salud", "Facultad de Ciencias", "Facultad de Ciencias Administrativas y Sociales", "Facultad de Ciencias Marinas", "Facultad de Deportes", "Facultad de Enología y Gastronomía", "Facultad de Idiomas", "Facultad de Ingeniería y Negocios - San Quintín", "Facultad de Ingeniería, Arquitectura y Diseño", "Instituto de Investigación y Desarrollo Educativo", "Instituto de Investigaciones Oceanológicas"]
     };
     
-    var selectedUbicacion = $("#ubicacion select").val();
-    const selectFacultad = $("#facultad select");
+    var selectedUbicacion = $("#txtCampus").val();
+    const selectFacultad = $("#txtFacultad");
 
     selectFacultad.empty();
 
     if (selectedUbicacion) {
         $('#txtFacultad').prop('disabled', false);
-        selectFacultad.append("<option value=''>Seleccionar Facultad</option>");
+        selectFacultad.append("<option value=''></option>");
         const facultades = facultadesXUbicacion[selectedUbicacion];
         facultades.forEach(facultad => {
             selectFacultad.append(`<option value='${facultad}'>${facultad}</option>`);
@@ -215,20 +258,22 @@ function Facultad() {
 }
 
 function Talleres() {
+    var selectedUbicacion = $("#txtCampus").val();
+    console.log("Selected campus:", selectedUbicacion);
     const talleresXUbicacion = {
         "Tijuana": ["Taller 1", "Taller 2", "Taller 3"],
         "Mexicali": ["Taller A", "Taller B", "Taller C"],
         "Ensenada": ["Taller X", "Taller Y", "Taller Z"]
     };
     
-    var selectedUbicacion = $("#ubicacion select").val();
-    const selectTaller = $("#taller select");
+    var selectedUbicacion = $("#txtCampus").val();
+    const selectTaller = $("#txtTaller");
 
     selectTaller.empty();
 
     if (selectedUbicacion) {
         $('#txtTaller').prop('disabled', false);
-        selectTaller.append("<option value=''>Seleccionar Taller</option>");
+        selectTaller.append("<option value=''></option>");
         const talleres = talleresXUbicacion[selectedUbicacion];
         talleres.forEach(taller => {
             selectTaller.append(`<option value='${taller}'>${taller}</option>`);
