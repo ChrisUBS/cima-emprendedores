3386c6fe2fd514350755e9ae63e48e2ed6faa4cb
Gael Ivan <gael.chavez@uabc.edu.mx>
2024-05-18 13:50:58 -0700

    Cambios visuales en el panel administrativo

--- BE/conection.php
+++ BE/conection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "1234";//Cambiar si tienes password en tu base de datos.
+$password = "";//Cambiar si tienes password en tu base de datos.
 $dbname = "cimarrones_emprendedores";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
